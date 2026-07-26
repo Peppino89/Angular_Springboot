7bfee075270c948246edc116b9829f4d45bb70c8
Giuseppe Giordano <lion8@hotmail.it>
2026-07-26 23:04:59 +0200

    update add register forn and login development

--- AppBarberShop/Frontend/barber-booking-frontend/src/app/features/auth/login/login.component.ts
+++ AppBarberShop/Frontend/barber-booking-frontend/src/app/features/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  private readonly fb = inject(FormBuilder);
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  readonly loading= signal<boolean>(false);
+  readonly errorMessage= signal<string | null>(null);
+  readonly showPassword= signal<boolean>(false);
+
+  readonly loginForm = this.fb.nonNullable.group({
+    username:['', [Validators.required]],
+  });
+
+}
